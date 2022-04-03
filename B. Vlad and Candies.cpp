4b161e27c2652bcf991b7aa3ccc39c74b6cd2902
DAISY-BLOSSOM <101879759+DAISY-BLOSSOM@users.noreply.github.com>
2022-04-03 15:55:52 +0200

    B. Vlad and Candies

--- B. Vlad and Candies.cpp
+++ B. Vlad and Candies.cpp
@@ -2,26 +2,26 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-	int t; string s;
-	cin>>t;
-	while(t--)
-	{
-		cin>>s;
-		int len=s.size();
-		map<char,int>m;
-		for(int i=0;i<s.size();i++)
-		{
-			m[s[i]]++;
-			if(m[s[i]]==2)
-			{
-				len-=2;
-				m.clear();
-			}
-		 } 
-		 cout<<len<<"\n";
-	}
-	return 0;
+ 
+int main() {
+  int t,s,n;
+   cin>>t;
+   while(t--){
+       cin>>n;
+       long long a[n];
+       for(int i=0;i<n;i++){
+           cin>>a[i];
+           }
+           if(n>1){
+                sort(a,a+n);
+                long long sub = a[n-1]-a[n-2];
+                if( sub>1)  cout<<"NO"<<endl;
+                else           cout<<"YES"<<endl;
+ 
+           }
+          else if(n==1){
+                if(a[n-1]>1)  cout<<"NO"<<endl;
+                else cout<<"YES"<<endl;
+          }
+   }
 }
