09e7efafac994dbd95e33c1c8167a71b46df5763
DAISY-BLOSSOM <101879759+DAISY-BLOSSOM@users.noreply.github.com>
2022-04-03 15:54:59 +0200

    C. Get an Even String

---
+++ C. Get an Even String.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/contest/1660/problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t; string s;
+	cin>>t;
+	while(t--)
+	{
+		cin>>s;
+		int len=s.size();
+		map<char,int>m;
+		for(int i=0;i<s.size();i++)
+		{
+			m[s[i]]++;
+			if(m[s[i]]==2)
+			{
+				len-=2;
+				m.clear();
+			}
+		 } 
+		 cout<<len<<"\n";
+	}
+	return 0;
+}
